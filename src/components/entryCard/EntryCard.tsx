38c0fea8ed1c3e8f688b87c502d7b049ddc2c5e4
Mark Lumba <Mark.Lumba.-ND@disney.com>
2025-07-31 11:47:27 -0400

    fix bugs

--- src/components/entryCard/EntryCard.tsx
+++ src/components/entryCard/EntryCard.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import type { CommonEntry } from "../../constants";
 import { ArrowUpRight } from "lucide-react";
 interface CommonEntryComp {
